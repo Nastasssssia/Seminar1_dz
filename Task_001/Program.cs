c646d48fd14ff75b717b037f9bf1c9840fe1eeb8
Nastassia <anastasia.goroninova@icloud.com>
2023-07-06 22:09:41 +0300

    добавили решение второй задачи

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -32,3 +32,32 @@ else
 }
 
 
+void Task2()
+
+{
+
+Console.WriteLine("Введите первое число");
+int num_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num_b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int num_c = Convert.ToInt32(Console.ReadLine());
+
+int max = num_a;
+if(num_b > max) 
+{
+   max = num_b; 
+}
+
+if(num_c > max)
+{
+    max = num_c;
+}
+
+Console.WriteLine("Наибольшее число:" + max);
+
+
+}
+
