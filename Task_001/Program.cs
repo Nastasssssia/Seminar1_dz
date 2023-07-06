7dc8be6a921b1291d1cf26800107f76752edbd44
Nastassia <anastasia.goroninova@icloud.com>
2023-07-06 22:46:35 +0300

    добавили решение 4 задачи

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -79,4 +79,19 @@ else
 
 }
 
-Task3();
+void Tast4()
+{
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до "+ n + ":");
+for(int i = 1; i <= n; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+}
